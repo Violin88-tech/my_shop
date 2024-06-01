2106568f82625ee970c6b843c5e78c1bc64a43c7
Viktoria <48157182+violin88@users.noreply.github.com>
2024-06-01 19:20:50 +0400

    исправила поиск

--- my_shop_project_test/pages/search_page.py
+++ my_shop_project_test/pages/search_page.py
@@ -13,11 +13,11 @@ class SearchPage:
 
     with allure.step("Input text for search negative"):
         def header_search_negative(self):
-            browser.element('.header__search input').type('asddfgrhtjykykk').press_enter()
+            browser.element('.header__search input').type('asddfgrhtjykykk8967868686').press_enter()
 
     with allure.step("Check the result of an unsuccessful search"):
         def search_result_failure(self):
-            browser.element('.search-type__text').should(have.text('По вашему запросу ничего не найдено. Возможно, вам понравятся эти '))
+            browser.element('.search-type__text').should(have.text('По вашему запросу ничего не найдено. Возможно, вам понравятся эти популярные товары'))
 
 
 search = SearchPage()
