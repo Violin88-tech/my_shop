7cd395452692db7bf6bea17e6e7473aa62b98f7d
Viktoria <48157182+violin88@users.noreply.github.com>
2024-06-05 21:36:35 +0400

    add time sleep

--- my_shop_project_test/pages/cart_page.py
+++ my_shop_project_test/pages/cart_page.py
@@ -10,7 +10,7 @@ class CartPage:
             browser.element(".header__search input").type(
                 "Искусственный интеллект с примерами на Python. Создание приложений искусственного интеллекта"
             ).press_enter()
-            time.sleep(3)
+            time.sleep(5)
             return self
     #
     with allure.step("Open product page"):
