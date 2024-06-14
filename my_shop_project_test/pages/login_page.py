89a97a1de040048cecf9e4ff5069496bf482b2d9
Viktoria <48157182+violin88@users.noreply.github.com>
2024-06-14 19:19:10 +0400

    добавила таймауты

--- my_shop_project_test/pages/login_page.py
+++ my_shop_project_test/pages/login_page.py
@@ -2,34 +2,33 @@ import time
 import allure
 from selene import browser, have, be, by
 import config
-
 class LoginPage:
     with allure.step("Open the authorization form"):
         def open_form(self):
             browser.element('.tabs-button[href="#"]').click()
-            browser.element('.popup-modal__window__header').with_(timeout=500).should(have.text(
+            browser.element('.popup-modal__window__header').with_(timeout=50).should(have.text(
                 'Вход и регистрация'))
             return self
 
     with allure.step("Open the authorization form with email"):
         def log_in_with_password(self):
             browser.element('.popup-modal__window').element(by.text('Войти по паролю')).click()
-            browser.element('.popup-modal__window').with_(timeout=500).should(have.text('Войти'))
+            browser.element('.popup-modal__window').with_(timeout=50).should(have.text('Войти'))
             return self
 
     with allure.step("Filling the authorization form email"):
         def fill_user_email(self):
-            browser.element('#email').should(be.blank).type(config.settings.USER_EMAIL).with_(timeout=200)
+            browser.element('#email').should(be.blank).type(config.settings.USER_EMAIL)
             return self
 
     with allure.step("Filling the authorization form positive"):
         def fill_password_positive(self):
-            browser.element('#pass').type(config.settings.PASSWORD).with_(timeout=200)
+            browser.element('#pass').type(config.settings.PASSWORD)
             return self
 
     with allure.step("Filling the authorization form password"):
         def fill_password_negative(self):
-            browser.element('#pass').type('123').with_(timeout=200)
+            browser.element('#pass').type('123')
             return self
 
     with allure.step("Submit the form"):
