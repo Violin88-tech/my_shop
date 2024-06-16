ea4c3a89161d6054173dcb9d9de66f39c7e516be
Viktoria <48157182+violin88@users.noreply.github.com>
2024-06-16 13:26:12 +0400

    добавила таймауты

--- my_shop_project_test/pages/favorite_page.py
+++ my_shop_project_test/pages/favorite_page.py
@@ -3,38 +3,45 @@ import allure
 from selene import browser, have
 
 class FavoritePage:
-    with allure.step("Number book search"):
-        def find_item(self):
-            browser.element('.header__search input').type(
+
+    @allure.step("Number book search")
+    def find_item(self):
+        #with allure.step("Number book search"):
+        browser.element('.header__search input').type(
                 "Искусственный интеллект с примерами на Python. Создание приложений искусственного интеллекта"
             ).press_enter()
-            browser.element('.header__search input').with_(timeout=200).should(have.value(
+        browser.element('.header__search input').with_(timeout=200).should(have.value(
                 "Искусственный интеллект с примерами на Python. Создание приложений искусственного интеллекта"))
-            return self
+        return self
 
-    with allure.step("Open product page"):
-        def open_page_item(self):
-            browser.element('.item .item__title').with_(timeout=50).click()
+    @allure.step("Open product page")
+    def open_page_item(self):
+
+        #with allure.step("Open product page"):
+        browser.element('.item .item__title').with_(timeout=50).click()
           #  browser.element('[to="#reviews"] [class="md:block"]').with_(timeout=50).should(have.text("Задать вопрос"))
-            return self
+        return self
 
-    with allure.step("Click add to favorites"):
-        def click_add_to_favorites(self):
-            browser.element('.form-control .is-heart').click()
-            browser.element('.favorite .badge').with_(timeout=200).should(have.exact_text('1'))
-            return self
+    @allure.step("Click add to favorites")
+    def click_add_to_favorites(self):
+        #with allure.step("Click add to favorites"):
+        browser.element('.form-control .is-heart').click()
+        browser.element('.favorite .badge').with_(timeout=200).should(have.exact_text('1'))
+        return self
 
-    with allure.step("Open favorites"):
-        def open_favorites(self):
-            browser.element('.header__icons .favorite').click()
-            browser.element('[class="is-desktop"]').with_(timeout=200).should(have.exact_text(
+    @allure.step("Open favorites")
+    def open_favorites(self):
+        #with allure.step("Open favorites"):
+        browser.element('.header__icons .favorite').click()
+        browser.element('[class="is-desktop"]').with_(timeout=200).should(have.exact_text(
                 'Перенести товары в наличии в корзину'))
-            return self
+        return self
 
-    with allure.step("Click delete to favorites"):
-        def click_delete_to_favorites(self):
-            browser.element('.item .is-heart').click()
-            time.sleep(3)
-            return self
+    @allure.step("Click delete to favorites")
+    def click_delete_to_favorites(self):
+        #with allure.step("Click delete to favorites"):
+        browser.element('.item .is-heart').click()
+        time.sleep(3)
+        return self
 
 favorite = FavoritePage()
