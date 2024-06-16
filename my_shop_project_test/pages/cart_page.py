2b98b711f1597f473276b3605861812cc0a6bcf8
Viktoria <48157182+violin88@users.noreply.github.com>
2024-06-16 14:25:50 +0400

    добавила таймауты

--- my_shop_project_test/pages/cart_page.py
+++ my_shop_project_test/pages/cart_page.py
@@ -15,36 +15,38 @@ class CartPage:
 
         @allure.step("Open product page")
         def open_page_item(self):
-            with allure.step("Open product page"):
-                browser.element('.item .item__title').click()
-                browser.element('[to="#reviews"] [class="md:block"]').with_(timeout=50).should(
+
+            #with allure.step("Open product page"):
+            browser.element('.item .item__title').click()
+            browser.element('[to="#reviews"] [class="md:block"]').with_(timeout=50).should(
                         have.text("Задать вопрос"))
-                return self
+            return self
 
         @allure.step("Click add to cart")
         def click_add_to_cart(self):
-           with allure.step("Click add to cart"):
+
+           #with allure.step("Click add to cart"):
             browser.element('.form-control>.field.flex-grow>button').click()
             browser.element('[href="/my/cart"] .badge').with_(timeout=100).should(have.exact_text('1'))
             return self
 
         @allure.step("Open cart")
         def open_cart(self):
-           with allure.step("Open cart"):
+           #with allure.step("Open cart"):
             browser.element('[href="/my/cart"]').click()
             browser.element('[class="cart-sort-pane__control"]').with_(timeout=50).should(have.text("Выбрать всё"))
             return self
 
         @allure.step("Clear cart")
         def clear_cart(self):
-           with allure.step("Clear cart"):
+         #  with allure.step("Clear cart"):
             browser.element('.cart-item .icon__delete').click()
             browser.element(".h2").with_(timeout=50).should(have.text("Удаление товаров"))
             return self
 
         @allure.step("Confirm clear cart")
         def confirm_clear_cart(self):
-          with allure.step("Confirm clear cart"):
+         # with allure.step("Confirm clear cart"):
             browser.element(' .popup-modal .cart-confirm__btns').element(by.text('Удалить')).click()
             browser.element('.wrap h3').with_(timeout=50).should(have.text('Ваша корзина пуста'))
             return self
