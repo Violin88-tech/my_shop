da0e71ab28fbb54f3ae2ba31f76af2d7787ceb82
Viktoria <48157182+violin88@users.noreply.github.com>
2024-06-16 14:04:54 +0400

    добавила таймауты

--- my_shop_project_test/pages/search_page.py
+++ my_shop_project_test/pages/search_page.py
@@ -3,13 +3,15 @@ import allure
 from selene import browser, have
 
 class SearchPage:
-    with allure.step("Input text for search positive"):
-        def header_search_positive(self):
-            browser.element('.header__search input').type('Тетради').press_enter()
-            browser.element('.header__search input').with_(timeout=200).should(have.value('Тетради'))
+    @allure.step("Input text for search positive")
+    #with allure.step("Input text for search positive"):
+    def header_search_positive(self):
+        browser.element('.header__search input').type('Тетради').press_enter()
+        browser.element('.header__search input').with_(timeout=200).should(have.value('Тетради'))
 
-    with allure.step("Input text for search negative"):
-        def header_search_negative(self):
-            browser.element('.header__search input').type('asddfgrhtjykykk8967868686').press_enter()
+    @allure.step("Input text for search negative")
+    #with allure.step("Input text for search negative"):
+    def header_search_negative(self):
+        browser.element('.header__search input').type('asddfgrhtjykykk8967868686').press_enter()
 
 search = SearchPage()
