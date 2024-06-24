3bb5b1cd854d4f9a00eb51b86026460693c2da2a
Viktoria <48157182+violin88@users.noreply.github.com>
2024-06-24 10:22:05 +0400

    добавила таймауты

--- tests/web/conftest.py
+++ tests/web/conftest.py
@@ -45,7 +45,7 @@ def browser_management():
         # driver_options.add_argument('--headless')
         browser.config.driver_options = driver_options
 
-        # browser.config.timeout = 4
+
 
         browser.config.window_width = 1920
         browser.config.window_height = 1080
@@ -61,15 +61,4 @@ def browser_management():
 
     browser.quit()
 
-# import pytest
-# from selene import browser
-# from selenium import webdriver
-#
-#
-# @pytest.fixture(scope='function', autouse=True)
-# def browser_management():
-#
-#
-#     yield
-#
-#     browser.quit()
+
